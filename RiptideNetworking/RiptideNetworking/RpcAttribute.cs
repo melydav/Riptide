d0a38e0053d489b7550d141eed8ffd31bdf0d091
Davee <melydav@proton.me>
2026-04-06 20:06:00 +0200

    Create RpcAttribute.cs

---
+++ RiptideNetworking/RiptideNetworking/RpcAttribute.cs
@@ -0,0 +1,17 @@
+﻿// This file isn't provided under The MIT License as a part of RiptideNetworking, rather an extension of it.
+// Copyright (c) Dávid "Davee" M.
+// For additional information on Riptide's licensing, please see the included LICENSE.md file or view it on GitHub:
+// https://github.com/RiptideNetworking/Riptide/blob/main/LICENSE.md
+
+using System;
+
+namespace Riptide
+{
+    /// <summary>Specifies a method as an Rpc method that can be called over the network.</summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class Rpc : Attribute
+    {
+        /// <summary>Initializes an Rpc.</summary>
+        public Rpc() { }
+    }
+}
